b307aa6223259508f5561784a74e4f7df7b7f5b0
Valdemar <ValdemarBing@gmail.com>
2023-04-13 23:41:10 +1200

    Add project files.

---
+++ EntryPoint/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntryPoint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
